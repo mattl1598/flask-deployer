43027eedc8362f35740eaa4601a784ceba8f0ab0
Matthew Larby <37371392+mattl1598@users.noreply.github.com>
2022-09-20 07:37:53 +0100

    fixing logs output
    Signed-off-by: Matthew Larby <37371392+mattl1598@users.noreply.github.com>

--- webapp/routes.py
+++ webapp/routes.py
@@ -235,7 +235,7 @@ def logs(project):
 			'-S', '-24h',
 			'-o', 'short-iso',
 			'-u', project
-		], check=True, capture_output=True, text=True)
+		], check=True, capture_output=True)
 		return str(proc.stdout)
 
 
